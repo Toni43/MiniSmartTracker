d011553f2c11216bc598ae2fac2813f52f9ea6a6
Toni43 <ra4nhy@yandex.ru>
2016-03-13 14:25:39 +0300

    Create config.h
    Вынесены основные настройки

---
+++ config.h
@@ -0,0 +1,15 @@
+// config file to defines callsign, ssid, symbols and home location
+#define MYCALL "RA4NHY"
+#define DEST_ADDR "APZ058"
+
+#define CALL_SSID 9
+// Refer to http://wa8lmf.net/aprs/APRS_symbols.htm
+#define SYMBOL_CHAR '('         // Blue Dot
+#define SYMBOL_TABLE 's'        // Primary Table (s) or Alternate Table (a)
+// Define your home lat & lon below
+const float HOME_LAT = 58.606806;
+const float HOME_LON = 49.570434;
+//
+//
+// Custom Comments here
+#define COMMENT "QAPRS SmartTrack"
