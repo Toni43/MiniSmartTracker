b38ac64306289d5e388479327c3ffb50d6689da9
Toni43 <ra4nhy@yandex.ru>
2016-03-13 21:03:44 +0300

    Update config.h

--- config.h
+++ config.h
@@ -4,9 +4,6 @@
 #define RELAY "WIDE2 2"
 #define CALL_SSID '9'
 
-// Refer to http://wa8lmf.net/aprs/APRS_symbols.htm
-#define SYMBOL_CHAR '('         // Blue Dot
-#define SYMBOL_TABLE 's'        // Primary Table (s) or Alternate Table (a)
 // Define your home lat & lon below
 const float HOME_LAT = 58.606806;
 const float HOME_LON = 49.570434;
